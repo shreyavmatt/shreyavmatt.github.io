ab351d000de801948b90b1e0c1e26986c75bb609
shreyavmatt <100143094+shreyavmatt@users.noreply.github.com>
2024-08-25 19:45:21 +0400

    Update portfolio-script.js

--- assets/js/portfolio-script.js
+++ assets/js/portfolio-script.js
@@ -175,8 +175,8 @@ window.addEventListener('scroll', function() {
   var value = window.scrollY;
 
   bg.style.right = value * 0.5 + 'px';
-  moon.style.left = -value * 0.5 + 'px';
-  mountain.style.top = -value * 0.15 + 'px';
+  moon.style.left = value * 0.5 + 'px';
+  mountain.style.top = value * 0.15 + 'px';
   road.style.top = value * 0.15 + 'px'
   text.style.top = value * 1 + 'px'
 
